51e42899d1104984c67aacc236eadb5f4c2c123b
Pavel <73703906+meow-or@users.noreply.github.com>
2021-09-27 22:07:34 +0500

    подключен скрипт main.js, написаны функции getRandomNumber, checkCommentLength

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const getRandomNumber = function (initialNumber, finiteNumber) {
+
+  //если на вход попало отрицательное значение - преобразуем его
+
+  if (initialNumber < 0) {
+    initialNumber = -initialNumber;
+  }
+  if (finiteNumber < 0) {
+    finiteNumber = -finiteNumber;
+  }
+
+  if (finiteNumber < initialNumber) {
+    // если конец < начала - меняем их местами (не указано, что именно нужно придумать, поэтому я придумал поменять их местами.)
+
+    let box = initialNumber;
+    initialNumber = finiteNumber;
+    finiteNumber = box;
+  }
+  // фрагмент с MDN
+  initialNumber = Math.ceil(initialNumber);
+  finiteNumber = Math.floor(finiteNumber);
+
+  return (Math.floor(Math.random() * (finiteNumber - initialNumber + 1)) + initialNumber);
+}
+
+getRandomNumber(200, 15);
+
+
+const checkCommentLength = function(comment, maxLength) {
+
+  if (comment.length <= maxLength) {
+    return true;
+  }
+  return false;
+}
+
+checkCommentLength('Функция для проверки максимальной длины строки', 140);
+
+
+
+
