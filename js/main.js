e5deb1472b505ff200e70ec4c6083c4411d11c43
Pavel <73703906+meow-or@users.noreply.github.com>
2021-09-28 21:23:28 +0500

    исправлены замечания, проведён рефакторинг

--- js/main.js
+++ js/main.js
@@ -1,37 +1,24 @@
-const getRandomNumber = function (initialNumber, finiteNumber) {
+const generateRandomNumber = function (init, fin) {
+  return Math.floor(Math.random() * (fin - init + 1)) + init;
+};
 
-  //если на вход попало отрицательное значение - преобразуем его
-
-  if (initialNumber < 0) {
-    initialNumber = -initialNumber;
-  }
-  if (finiteNumber < 0) {
-    finiteNumber = -finiteNumber;
+const getRandomNumber = function (minNumber, maxNumber) {
+  if (minNumber < 0 || maxNumber < 0) {
+    throw new Error('начало или конец диапазона не могут быть отрицательными');
   }
 
-  if (finiteNumber < initialNumber) {
-    // если конец < начала - меняем их местами (не указано, что именно нужно придумать, поэтому я придумал поменять их местами.)
+  const min = Math.ceil(minNumber);
+  const max = Math.floor(maxNumber);
 
-    const box = initialNumber;
-    initialNumber = finiteNumber;
-    finiteNumber = box;
-  }
-  // фрагмент с MDN
-  initialNumber = Math.ceil(initialNumber);
-  finiteNumber = Math.floor(finiteNumber);
-
-  return (Math.floor(Math.random() * (finiteNumber - initialNumber + 1)) + initialNumber);
+  return min > max
+    ? generateRandomNumber(max, min)
+    : generateRandomNumber(min, max);
 };
 
 getRandomNumber(200, 15);
 
-
-const checkCommentLength = function(comment, maxLength) {
-
-  if (comment.length <= maxLength) {
-    return true;
-  }
-  return false;
+const checkCommentLength = function (comment, maxLength) {
+  return comment.length <= maxLength;
 };
 
 checkCommentLength('Функция для проверки максимальной длины строки', 140);
