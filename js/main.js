678bb479086cdc0bec497fda25b56f05ea28a8ec
Pavel <73703906+meow-or@users.noreply.github.com>
2021-10-10 19:41:10 +0500

    в util.js вынесены функции генерации чисел и проверки длины строки

--- js/main.js
+++ js/main.js
@@ -1,48 +1,4 @@
-
-const generateRandomNumber = function (init, fin) {
-  return Math.floor(Math.random() * (fin - init + 1)) + init;
-};
-
-const getRandomNumber = function (minNumber, maxNumber) {
-  if (minNumber < 0 || maxNumber < 0) {
-    throw new Error('начало или конец диапазона не могут быть отрицательными');
-  }
-
-  const min = Math.ceil(minNumber);
-  const max = Math.floor(maxNumber);
-
-  return min > max
-    ? generateRandomNumber(max, min)
-    : generateRandomNumber(min, max);
-};
-
-
-const checkCommentLength = function (comment, maxLength) {
-  return comment.length <= maxLength;
-};
-
-checkCommentLength('Функция для проверки максимальной длины строки", 140');
-
-
-const createRandomIdFromRange = function (min, max) {
-  const previousValues = [];
-
-  return function () {
-    let currentValue = getRandomNumber(min, max);
-
-    if (previousValues.length >= (max - min + 1)) {
-      throw new Error(`Перебраны все числа из диапазона от ${min} до ${max}`);
-    }
-
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomNumber(min, max);
-    }
-
-    previousValues.push(currentValue);
-
-    return currentValue;
-  };
-};
+import { getRandomNumber, createRandomIdFromRange } from './util.js';
 
 const COMMENTS = [
   'Всё отлично!',
@@ -84,5 +40,4 @@ const createPhotoDescription = function () {
 
 const listOfPhotoDescriptions = Array.from({ length: DESCRIPTIONS_COUNT }, createPhotoDescription);
 
-listOfPhotoDescriptions[0]; //vs eslint
-
+console.log(listOfPhotoDescriptions);
